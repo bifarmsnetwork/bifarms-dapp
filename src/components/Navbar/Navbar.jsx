faa304bfbdda36785ce884138a2bcc30b2a36662
Punith Nandiraj <punithnandiraj@gmail.com>
2022-04-05 19:23:39 +0530

    Few changes

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.css';
 import Logo from '../../images/logo.png';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { GrClose } from 'react-icons/gr';
 
 
 const Navbar = () => {
+    const [isMobile , setIsMobile]  = useState(true);
   return (
     <div className="header">
         <div className="header-container">
             <div className="header-left">
             <img src={Logo} alt="Logo" className='header-left-logo' />
-                <div className="nav">
+            
+                <div className={isMobile ? "nav-mobile" : "nav"}
+                    onClick={() => setIsMobile(false)}
+                >
                     <div className="n-item"> Launchpad </div>
                     <div className="n-item"> Farm </div>
                     <div className="n-item"> Pool </div>
@@ -22,6 +28,10 @@ const Navbar = () => {
                 </div>
                 
             </div>
+            <button className="mobile-menu-icon">
+                {isMobile ? <GiHamburgerMenu/> : <GrClose/>}
+            </button>
+            
         </div>
     </div>
   )
