dd9282e386b8beed368f290e1402779a1e1c9228
rithgan <rithikgandhi1989@gmail.com>
2022-04-10 16:21:23 +0530

    added other wallets

--- src/modal.js
+++ src/modal.js
@@ -1,5 +1,7 @@
 import Web3Modal from "web3modal";
 import { ethers } from "ethers";
+import Authereum from "authereum";
+import WalletConnectProvider from '@walletconnect/web3-provider'
 
 const providerOptions = {
   /* See Provider Options Section */
@@ -15,6 +17,31 @@ const providerOptions = {
   //       infuraId: '0x1E6b54f079157E58CC9b116F924B384509b63F82' // required
   //     }
   //   }
+  walletconnect: {
+			package: WalletConnectProvider, // required
+			options: {
+				infuraId: '0x1E6b54f079157E58CC9b116F924B384509b63F82', //will support the following Mainnet (1), Ropsten (3), Rinkeby(4), Goerli (5) and Kovan (42)
+				network: 'mainnet',
+				qrcodeModalOptions: {
+					mobileLinks: [
+						'rainbow',
+						'metamask',
+						'argent',
+						'trust',
+						'imtoken',
+						'pillar',
+						'safepal',
+					],
+					desktopLinks: ['encrypted ink'],
+				},
+			},
+		},
+  authereum: {
+    package: Authereum // required
+  },
+  binancechainwallet: {
+    package: true
+  }
 };
 
 export const web3Modal = () =>
