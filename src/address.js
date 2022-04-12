c62803bf434ccd36c2b98acfd32ded6b31435452
rithgan <rithikgandhi1989@gmail.com>
2022-04-12 14:17:36 +0530

    comments for address

--- src/address.js
+++ src/address.js
@@ -1,2 +1,2 @@
-export const token = "0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56"
-export const address = "0xE12808316c2fd95eEf14a45745E9F176Ff710b94"
+export const token = "0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56" //busd mainnet
+export const address = "0xE12808316c2fd95eEf14a45745E9F176Ff710b94" //bifarms seedsale mainnet
