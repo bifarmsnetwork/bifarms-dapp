d6ea5b36787c20bbc37e0c9b063af97cde296fe7
rithgan <rithikgandhi1989@gmail.com>
2022-04-26 00:28:41 +0530

    address abi airdrop mainet

--- src/address.js
+++ src/address.js
@@ -1,2 +1,3 @@
 export const token = "0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56" //busd mainnet
 export const address = "0xE12808316c2fd95eEf14a45745E9F176Ff710b94" //bifarms seedsale mainnet
+export const airdrop = "0xC1104F74c67ACf945Eeafc35C640B3089aEF9dAe" //bifarms airdrop mainet
