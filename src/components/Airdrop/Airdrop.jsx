5eb6e3c34131662021bc08f4c16ce4b266b6f201
rithgan <rithikgandhi1989@gmail.com>
2022-04-26 10:32:24 +0530

    token address in airdrop

--- src/components/Airdrop/Airdrop.jsx
+++ src/components/Airdrop/Airdrop.jsx
@@ -46,8 +46,8 @@ const Airdrop = () => {
                           <span className="color_grey">
                               Token Address:
                           </span>{" "}
-                            <a className = "theme_color color_hover" target="_blank" href="https://bscscan.com/address/0xbbf8c8f9b79808be0bc8d24f2c7a5111b47e230b#code">0xbbf8c8f9b79808be0bc8d24f2c7a5111b47e230b
-                          <span style={{display: "none"}}>{account}</span>
+                            <a className = "theme_color color_hover" target="_blank" href="https://bscscan.com/address/0xbbf8c8f9b79808be0bc8d24f2c7a5111b47e230b#code">
+                          0xbbf8c8f9b79808be0bc8d24f2c7a5111b47e230b
                             </a>
                         </p>
                       </div>
