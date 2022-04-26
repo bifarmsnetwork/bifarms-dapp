4ec0e6f93b8ef33bf79ba33a38f8cc74f18a7968
rithgan <rithikgandhi1989@gmail.com>
2022-04-26 15:59:23 +0530

    address mainnet

--- src/address.js
+++ src/address.js
@@ -1,3 +1,3 @@
-export const token = "0xAC212351dC5E1d056D5a36E00A62BA748CbF694c" //busd testnet
-export const address = "0x1868819e052D2daA9a9e770AdB9e175188343971" //bifarms seedsale testnet
-export const airdrop = "0x9eB90Ad27bBb96c80bb41D0712c4B3D580AcC2F4" //bifarms airdrop tesnet
+export const token = "0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56" //busd mainnet
+export const address = "0xE12808316c2fd95eEf14a45745E9F176Ff710b94" //bifarms seedsale mainnet
+export const airdrop = "0xC1104F74c67ACf945Eeafc35C640B3089aEF9dAe" //bifarms airdrop mainet
